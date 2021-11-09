79e65d2719c510cd411b3f787fc2ccd0474bf060
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-09 15:49:22 +0000

    unit test for spaces

--- spec/spaces_spec.rb
+++ spec/spaces_spec.rb
@@ -3,7 +3,9 @@ require 'spaces'
 describe Spaces do
   describe '.all' do
     it 'returns a list of spaces' do
-      Spaces.all
+      spaces = Spaces.all
+      expect(spaces).to include 'Haunted hill'
+
     end
   end
 end

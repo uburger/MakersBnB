ad2baab40f89ee9aef734fc2653a4b8b02b8cf87
michael steinacher <steinacherm@hotmail.co.uk>
2021-11-10 17:21:53 +0000

    Updated login and sign up feature tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,14 +4,4 @@ feature 'Sign up' do
 
     expect(page).to have_content 'Sign-up'
   end
-
-  scenario 'User enters details and submits them' do
-    visit 'sign-up'
-    fill_in('email', with: 'example@example.com')
-    fill_in('password', with: 'password')
-    click_button 'Submit'
-
-    expect(page).to have_content 'example@example.com'
-    expect(page).to have_content 'password'
-  end
 end
